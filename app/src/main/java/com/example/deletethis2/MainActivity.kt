cc4bea4fbc0cfefd9608fadf32ea46819423add3
AishwaryaKamal <aishwaryakamal5@gmail.com>
2020-09-17 02:09:47 +0530

    Added commented stuff

--- app/src/main/java/com/example/deletethis2/MainActivity.kt
+++ app/src/main/java/com/example/deletethis2/MainActivity.kt
@@ -156,3 +156,117 @@ class MainActivity : AppCompatActivity() {
 //    }
 //}
 
+
+
+
+//package infix.studios.deletethis2
+//
+//import android.graphics.Color
+//import android.os.Bundle
+//import android.view.View
+//import android.widget.TextView
+//import androidx.appcompat.app.AppCompatActivity
+//import com.chinalwb.are.AREditor
+//import jp.wasabeef.richeditor.RichEditor
+//
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    private var mEditor: RichEditor? = null
+////    private var mPreview: TextView? = null
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        mEditor = findViewById<View>(R.id.editor) as RichEditor
+//
+//        mEditor!!!!.setEditorHeight(200)
+//        mEditor!!.setEditorFontSize(22)
+//        mEditor!!.setEditorFontColor(Color.RED)
+//        //mEditor!!.setEditorBackgroundColor(Color.BLUE);
+//        //mEditor!!.setBackgroundColor(Color.BLUE);
+//        //mEditor!!.setBackgroundResource(R.drawable.bg);
+//        mEditor!!.setPadding(10, 10, 10, 10)
+//        //mEditor!!.setBackground("https://raw.githubusercontent.com/wasabeef/art/master/chip.jpg");
+//        mEditor!!.setPlaceholder("Insert text here...")
+//        //mEditor!!.setInputEnabled(false);
+////        mPreview = findViewById<View>(R.id.preview) as TextView
+//        mEditor!!.setOnTextChangeListener(object : RichEditor.OnTextChangeListener {
+//            override fun onTextChange(text: String?) {
+////                mPreview!!.text = text
+//            }
+//        })
+//        findViewById<View>(R.id.action_undo).setOnClickListener { mEditor!!.undo() }
+//        findViewById<View>(R.id.action_redo).setOnClickListener { mEditor!!.redo() }
+//        findViewById<View>(R.id.action_bold).setOnClickListener { mEditor!!.setBold() }
+//        findViewById<View>(R.id.action_italic).setOnClickListener { mEditor!!.setItalic() }
+//        findViewById<View>(R.id.action_subscript).setOnClickListener { mEditor!!.setSubscript() }
+//        findViewById<View>(R.id.action_superscript).setOnClickListener { mEditor!!.setSuperscript() }
+//        findViewById<View>(R.id.action_strikethrough).setOnClickListener { mEditor!!.setStrikeThrough() }
+//        findViewById<View>(R.id.action_underline).setOnClickListener { mEditor!!.setUnderline() }
+//        findViewById<View>(R.id.action_heading1).setOnClickListener {
+//            mEditor!!.setHeading(
+//                1
+//            )
+//        }
+//        findViewById<View>(R.id.action_heading2).setOnClickListener {
+//            mEditor!!.setHeading(
+//                2
+//            )
+//        }
+//        findViewById<View>(R.id.action_heading3).setOnClickListener {
+//            mEditor!!.setHeading(
+//                3
+//            )
+//        }
+//        findViewById<View>(R.id.action_heading4).setOnClickListener {
+//            mEditor!!.setHeading(
+//                4
+//            )
+//        }
+//        findViewById<View>(R.id.action_heading5).setOnClickListener {
+//            mEditor!!.setHeading(
+//                5
+//            )
+//        }
+//        findViewById<View>(R.id.action_heading6).setOnClickListener {
+//            mEditor!!.setHeading(
+//                6
+//            )
+//        }
+//        findViewById<View>(R.id.action_txt_color).setOnClickListener(object : View.OnClickListener {
+//            private var isChanged = false
+//            override fun onClick(v: View) {
+//                mEditor!!.setTextColor(if (isChanged) Color.BLACK else Color.RED)
+//                isChanged = !isChanged
+//            }
+//        })
+//        findViewById<View>(R.id.action_bg_color).setOnClickListener(object : View.OnClickListener {
+//            private var isChanged = false
+//            override fun onClick(v: View) {
+//                mEditor!!.setTextBackgroundColor(if (isChanged) Color.TRANSPARENT else Color.YELLOW)
+//                isChanged = !isChanged
+//            }
+//        })
+//        findViewById<View>(R.id.action_indent).setOnClickListener { mEditor!!.setIndent() }
+//        findViewById<View>(R.id.action_outdent).setOnClickListener { mEditor!!.setOutdent() }
+//        findViewById<View>(R.id.action_align_left).setOnClickListener { mEditor!!.setAlignLeft() }
+//        findViewById<View>(R.id.action_align_center).setOnClickListener { mEditor!!.setAlignCenter() }
+//        findViewById<View>(R.id.action_align_right).setOnClickListener { mEditor!!.setAlignRight() }
+//        findViewById<View>(R.id.action_blockquote).setOnClickListener { mEditor!!.setBlockquote() }
+//        findViewById<View>(R.id.action_insert_bullets).setOnClickListener { mEditor!!.setBullets() }
+//        findViewById<View>(R.id.action_insert_numbers).setOnClickListener { mEditor!!.setNumbers() }
+//        findViewById<View>(R.id.action_insert_image).setOnClickListener {
+//            mEditor!!.insertImage(
+//                "http://www.1honeywan.com/dachshund/image/7.21/7.21_3_thumb.JPG",
+//                "dachshund"
+//            )
+//        }
+//        findViewById<View>(R.id.action_insert_link).setOnClickListener {
+//            mEditor!!.insertLink(
+//                "https://github.com/wasabeef",
+//                "wasabeef"
+//            )
+//        }
+//        findViewById<View>(R.id.action_insert_checkbox).setOnClickListener { mEditor!!.insertTodo() }
+//    }
+//}
